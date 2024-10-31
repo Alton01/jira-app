43368a1023a35638e9699a2a9327e7278b58cfe0
Altono1 <altonobinna11@gmail.com>
2024-10-31 16:56:25 +0100

    implement task components and description functionality

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { DottedSeparator } from "@/components/dotted-separator";
+import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { TaskBreadCrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { TaskDescription } from "@/features/tasks/components/task-description";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
 export const TaskIdClient = () => {
@@ -11,9 +16,18 @@ export const TaskIdClient = () => {
   if (isLoading) {
     return <PageLoader />;
   }
+
+  if (!data) {
+    return <PageError message="Task not found!!" />;
+  }
   return (
-    <div>
-      <p>Task id client</p>
+    <div className="flex flex-col">
+      <TaskBreadCrumbs project={data.project} task={data} />
+      <DottedSeparator className="my-6" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <TaskOverview task={data} />
+        <TaskDescription task={data} />
+      </div>
     </div>
   );
 };
